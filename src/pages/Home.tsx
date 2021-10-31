405804a67244601b8252a70f11b0d09517b9c0f8
danvb112 <danielbonasser@gmail.com>
2021-10-31 14:39:49 -0300

    Add Auth page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,38 @@
+
+import illustrationImg from '../assets/images/illustration.svg';
+import logoImg from '../assets/images/logo.svg';
+import googleIcon from '../assets/images/google-icon.svg';
+
+import '../styles/auth.scss';
+import { Button } from '../components/Button';
+
+export function Home() {
+    return (
+        <div id="page-auth">
+            <aside>
+                <img src={illustrationImg} alt="Ilustarção sobre criação de salas" />
+                <strong>Crie Salas de Q&amp;A ao vivo</strong>
+                <p>Tire as dúvidas de sua audiência em tempo real</p>
+            </aside>
+            <main>
+                <div className="main-content">
+                    <img src={logoImg} alt="Logo Letmeask"/>
+                    <button className="create-room">
+                        <img src={googleIcon} alt="Google icon"/>
+                        Crie sua sala com o Google
+                    </button>
+                    <div className="separator">ou entre em uma sala</div>
+                    <form>
+                        <input 
+                            type='text'
+                            placeholder='Digite o código da sala'
+                        />
+                        <Button type='submit'>
+                            Entrar na sala
+                        </Button>
+                    </form>
+                </div>
+            </main>
+        </div>
+    )
+}

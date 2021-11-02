f4c14110c5848c614b58766e949b1eddda492eab
danvb112 <danielbonasser@gmail.com>
2021-11-02 10:01:59 -0300

    Add Routes between Home and NewRoom

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,4 +1,6 @@
 
+import { Link } from 'react-router-dom'
+
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIcon from '../assets/images/google-icon.svg';
@@ -29,7 +31,7 @@ export function NewRoom() {
                     </form>
                     <p>
                         Quer entrar em uma sala já existente? 
-                        <a href='#'>Clique aqui</a>
+                        <Link to='/'>Clique aqui</Link>
                     </p>
                 </div>
             </main>
